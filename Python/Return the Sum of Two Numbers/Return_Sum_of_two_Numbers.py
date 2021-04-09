19d714f0ff9804c03d298f947b25f7c88845588e
munyuajojo <jnmunyua@gmail.com>
2021-04-09 19:42:56 +0300

    Return_sum_of_two_Numbers'

---
+++ Python/Return the Sum of Two Numbers/Return_Sum_of_two_Numbers.py
@@ -0,0 +1,23 @@
+"""
+Return the Sum of Two Numbers
+Create a function that takes two numbers as arguments and return their sum.
+
+Examples
+addition(3, 2) ➞ 5
+
+addition(-3, -6) ➞ -9
+
+addition(7, 3) ➞ 10
+"""
+
+#Solution
+def addition(a,b):
+    
+    result = a+b
+    return result
+
+a=3
+b=2
+	
+addition_result= addition (a,b)
+print (addition_result)
